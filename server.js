977fbb5f06d5bbcc631e07f2e262a7066f525d51
RealTejasRao <ytejas72@gmail.com>
2026-01-03 15:22:39 +0530

    add healthcheck, connect mongoDB with mongoose, modify utils

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 dotenv.config();
 import app from "./src/app.js";
-
-dotenv.config({
-  path: "./.env", //looks for a .env file in the root directory
-});
+import connectDB from "./src/config/db.js";
 
 const port = process.env.PORT || 3000;
+
+connectDB();
+
 app.listen(port, () => {
   console.log(`Listening on port http://localhost:${port}`);
 });
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 
+
 const app = express();
 
 //basic configurations for express
@@ -8,9 +9,7 @@ app.use(express.json({ limit: "16kb" })); //allows Express to read JSON safely a
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //It lets Express read form submissions, including nested data.
 app.use(express.static("public")); // It exposes the public folder so browsers can directly access static files.
 
-app.get("/", (req, res) => {
-  res.send("TEJAS RAO");
-});
+
 
 // cors config
 const allowedOrigins = process.env.CORS_ORIGIN
@@ -26,4 +25,13 @@ app.use(
   }),
 );
 
+//import routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+
+
+app.get("/", (req, res) => {
+  res.send("TEJAS RAO");
+});
 export default app;
